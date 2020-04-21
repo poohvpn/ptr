722f60a8fdad992f5c0da821950bc72428c1ed14
honeycruse <ilovehoney@cruse.xxx>
2020-04-21 01:20:47 -0400

    use pinter casting

--- new.go
+++ new.go
@@ -5,86 +5,69 @@ import (
 )
 
 func NewBool(v bool) *Bool {
-	p := Bool(v)
-	return &p
+	return (*Bool)(&v)
 }
 
 func NewUint8(v uint8) *Uint8 {
-	p := Uint8(v)
-	return &p
+	return (*Uint8)(&v)
 }
 
 func NewUint16(v uint16) *Uint16 {
-	p := Uint16(v)
-	return &p
+	return (*Uint16)(&v)
 }
 
 func NewUint32(v uint32) *Uint32 {
-	p := Uint32(v)
-	return &p
+	return (*Uint32)(&v)
 }
 
 func NewUint64(v uint64) *Uint64 {
-	p := Uint64(v)
-	return &p
+	return (*Uint64)(&v)
 }
 
 func NewInt8(v int8) *Int8 {
-	p := Int8(v)
-	return &p
+	return (*Int8)(&v)
 }
 
 func NewInt16(v int16) *Int16 {
-	p := Int16(v)
-	return &p
+	return (*Int16)(&v)
 }
 
 func NewInt32(v int32) *Int32 {
-	p := Int32(v)
-	return &p
+	return (*Int32)(&v)
 }
 
 func NewInt64(v int64) *Int64 {
-	p := Int64(v)
-	return &p
+	return (*Int64)(&v)
 }
 
 func NewFloat32(v float32) *Float32 {
-	p := Float32(v)
-	return &p
+	return (*Float32)(&v)
 }
 
 func NewFloat64(v float64) *Float64 {
-	p := Float64(v)
-	return &p
+	return (*Float64)(&v)
 }
 
 func NewComplex64(v complex64) *Complex64 {
-	p := Complex64(v)
-	return &p
+	return (*Complex64)(&v)
 }
 
 func NewComplex128(v complex128) *Complex128 {
-	p := Complex128(v)
-	return &p
+	return (*Complex128)(&v)
 }
 
 func NewString(v string) *String {
-	p := String(v)
-	return &p
+	return (*String)(&v)
 }
 
 func NewInt(v int) *Int {
-	p := Int(v)
-	return &p
+	return (*Int)(&v)
 }
 
 func NewUint(v uint) *Uint {
-	p := Uint(v)
-	return &p
+	return (*Uint)(&v)
 }
 
 func NewTime(v time.Time) *Time {
-	p := Time(v)
-	return &p
+	return (*Time)(&v)
 }
