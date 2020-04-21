ab1b3f04e820b9dfdfd19b92c5e928b331351a87
honeycruse <ilovehoney@cruse.xxx>
2020-04-20 23:24:45 -0400

    support String()

--- pointer.go
+++ pointer.go
@@ -1,6 +1,7 @@
 package ptr
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -31,11 +32,19 @@ func (p *Bool) Value() (res bool) {
 	return
 }
 
+func (p *Bool) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Uint8) Value() (res uint8) {
 	if p != nil {
 		return uint8(*p)
 	}
 	return
+}
+
+func (p *Uint8) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *Uint16) Value() (res uint16) {
@@ -45,11 +54,19 @@ func (p *Uint16) Value() (res uint16) {
 	return
 }
 
+func (p *Uint16) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Uint32) Value() (res uint32) {
 	if p != nil {
 		return uint32(*p)
 	}
 	return
+}
+
+func (p *Uint32) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *Uint64) Value() (res uint64) {
@@ -59,11 +76,19 @@ func (p *Uint64) Value() (res uint64) {
 	return
 }
 
+func (p *Uint64) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Int8) Value() (res int8) {
 	if p != nil {
 		return int8(*p)
 	}
 	return
+}
+
+func (p *Int8) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *Int16) Value() (res int16) {
@@ -73,11 +98,19 @@ func (p *Int16) Value() (res int16) {
 	return
 }
 
+func (p *Int16) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Int32) Value() (res int32) {
 	if p != nil {
 		return int32(*p)
 	}
 	return
+}
+
+func (p *Int32) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *Int64) Value() (res int64) {
@@ -87,11 +120,19 @@ func (p *Int64) Value() (res int64) {
 	return
 }
 
+func (p *Int64) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Float32) Value() (res float32) {
 	if p != nil {
 		return float32(*p)
 	}
 	return
+}
+
+func (p *Float32) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *Float64) Value() (res float64) {
@@ -101,6 +142,10 @@ func (p *Float64) Value() (res float64) {
 	return
 }
 
+func (p *Float64) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Complex64) Value() (res complex64) {
 	if p != nil {
 		return complex64(*p)
@@ -108,11 +153,19 @@ func (p *Complex64) Value() (res complex64) {
 	return
 }
 
+func (p *Complex64) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Complex128) Value() (res complex128) {
 	if p != nil {
 		return complex128(*p)
 	}
 	return
+}
+
+func (p *Complex128) String() string {
+	return fmt.Sprint(p.Value())
 }
 
 func (p *String) Value() (res string) {
@@ -129,6 +182,10 @@ func (p *Int) Value() (res int) {
 	return
 }
 
+func (p *Int) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Uint) Value() (res uint) {
 	if p != nil {
 		return uint(*p)
@@ -136,9 +193,17 @@ func (p *Uint) Value() (res uint) {
 	return
 }
 
+func (p *Uint) String() string {
+	return fmt.Sprint(p.Value())
+}
+
 func (p *Time) Value() (res time.Time) {
 	if p != nil {
 		return time.Time(*p)
 	}
 	return
+}
+
+func (p *Time) String() string {
+	return string(p.Value().AppendFormat(make([]byte, 0, len(time.RFC3339Nano)), time.RFC3339Nano))
 }
--- pointer_test.go
+++ pointer_test.go
@@ -3,6 +3,7 @@ package ptr
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -31,4 +32,45 @@ func TestJsonUnMarshal(t *testing.T) {
 	as.NoError(err)
 	as.Equal(`anna`, model.Name.Value())
 	as.Zero(model.Age.Value())
+}
+
+func TestString(t *testing.T) {
+	as := assert.New(t)
+	as.Equal("true", NewBool(true).String())
+	as.Equal("false", NewBool(false).String())
+	as.Equal("-1", NewInt(-1).String())
+	as.Equal("0", NewInt(0).String())
+	as.Equal("1", NewInt(1).String())
+	as.Equal("-1", NewInt8(-1).String())
+	as.Equal("0", NewInt8(0).String())
+	as.Equal("1", NewInt8(1).String())
+	as.Equal("-1", NewInt16(-1).String())
+	as.Equal("0", NewInt16(0).String())
+	as.Equal("1", NewInt16(1).String())
+	as.Equal("-1", NewInt32(-1).String())
+	as.Equal("0", NewInt32(0).String())
+	as.Equal("1", NewInt32(1).String())
+	as.Equal("-1", NewInt64(-1).String())
+	as.Equal("0", NewInt64(0).String())
+	as.Equal("1", NewInt64(1).String())
+	as.Equal("0", NewUint(0).String())
+	as.Equal("1", NewUint(1).String())
+	as.Equal("0", NewUint8(0).String())
+	as.Equal("1", NewUint8(1).String())
+	as.Equal("0", NewUint16(0).String())
+	as.Equal("1", NewUint16(1).String())
+	as.Equal("0", NewUint32(0).String())
+	as.Equal("1", NewUint32(1).String())
+	as.Equal("0", NewUint64(0).String())
+	as.Equal("1", NewUint64(1).String())
+	as.Equal("1", NewFloat32(1.0).String())
+	as.Equal("-1.1", NewFloat32(-1.1).String())
+	as.Equal("1", NewFloat64(1.0).String())
+	as.Equal("-1.1", NewFloat64(-1.1).String())
+	as.Equal("(-1.1+1i)", NewComplex64(-1.1+1i).String())
+	as.Equal("(1.1+0i)", NewComplex64(1.1).String())
+	as.Equal("0001-01-01T00:00:00Z", NewTime(time.Time{}).String())
+	as.Equal("2020-02-02T02:02:02.000000002Z", NewTime(
+		time.Date(2020, 2, 2, 2, 2, 2, 2, time.UTC),
+	).String())
 }
